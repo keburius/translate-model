6cf04b3bc197d493f47188f19471108a50ea0746
Giorgi Keburia <keburius@Giorgis-MacBook-Pro.local>
2023-07-27 18:09:03 +0400

    switched

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from transformers import AutoModelForSeq2SeqLM, MT5Tokenizer
 
 
 class TranslationModel:
-    def __init__(self, model_repo='google/mt5-base', model_path='/Users/keburius/Desktop/NLP/Models/v3-ss-tr'):
+    def __init__(self, model_repo='google/mt5-base', model_path='/home/ubuntu/model/v2-ss-tr'):
         self.LANG_TOKEN_MAPPING = {
             'DescriptionEn': '<en>',
             'DescriptionGe': '<ka>',
